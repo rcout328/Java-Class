fbc6a0c45b24e518511bdf5b200ca5ff3059a503
rcout328 <solovpxoffical@gmail.com>
2024-03-01 20:16:05 +0530

    commit

---
+++ factorial.java
@@ -0,0 +1,11 @@
+public class factorial {
+    public static void main(String[] args) {
+        int num = Integer.parseInt(args[0]);
+        int fact=1;
+
+        for(int i=0;i<=num;i++){
+                fact = num*1;
+        }
+        System.out.println(fact);
+    }
+}
