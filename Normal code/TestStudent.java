603c6d9bed9847bb28f3c2e3343b0281609bec97
rcout328 <solovpxoffical@gmail.com>
2024-03-15 10:52:26 +0530

    c

--- Normal code/TestStudent.java
+++ Normal code/TestStudent.java
@@ -4,18 +4,20 @@ import java.util.Scanner;
 class student{
     String name;
     int id;
-
+    int age;
     student(){
         name =  "Abc";
         id = 11;
+        age =  22;
     }
-    student(int id ,String name){
-        name =  name;
-        id = id;
+    student(int id ,String name,int age){
+        this.name =  name;
+        this.id = id;
+        this.age = age;
     }
     public void display() {
         
-        System.out.println( "id = "+id + " " + "Name = "+ name );
+        System.out.println( "id = "+id + " " + "Name = "+ name + "age = " +age);
         
     }
 }
@@ -29,7 +31,9 @@ public class TestStudent {
         s1.display();
         System.out.println("Enter your id");
         int i= sc.nextInt();
-        student s2 = new student(i,s);
+        System.out.println("Enter your age");
+        int l= sc.nextInt();
+        student s2 = new student(i,s,l);
         s2.display();
     }
     
